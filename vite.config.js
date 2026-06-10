5a7e0d0f8c9ba7f7595242d847780f4a5c7c1f7e
w8s3suimin <w8s3suimin@users.noreply.github.com>
2026-06-10 18:17:16 +0800

    Auto-update site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
+  plugins: [
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      webp: { lossy: true, quality: 80 },
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
